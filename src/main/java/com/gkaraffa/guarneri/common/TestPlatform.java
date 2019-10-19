171554906ae59fe37c3a1536e15bd94d454e228f
Gabriel D. Karaffa <gk71pro@gmail.com>
2019-10-19 00:07:29 -0400

    commit point

--- src/main/java/com/gkaraffa/guarneri/common/TestPlatform.java
+++ src/main/java/com/gkaraffa/guarneri/common/TestPlatform.java
@@ -1,6 +1,8 @@
 package com.gkaraffa.guarneri.common;
 
-import com.gkaraffa.cremona.helper.Helper;
+import com.gkaraffa.cremona.helper.ChordHelper;
+import com.gkaraffa.cremona.helper.ScaleHelper;
+import com.gkaraffa.cremona.theoretical.chord.Chord;
 import com.gkaraffa.cremona.theoretical.scale.Scale;
 import com.gkaraffa.guarneri.outputform.GridTextOutputFormFactory;
 import com.gkaraffa.guarneri.outputform.OutputForm;
@@ -17,23 +19,24 @@ public class TestPlatform {
   public TestPlatform() {}
 
   public static void main(String[] args) {
-    Helper helper = Helper.getInstance();
+    ScaleHelper helper = ScaleHelper.getInstance();
     Scale scale = helper.getScale("C", "Ionian");
     ViewFactory modelFactory = new RomanNumeralAnalyticViewFactory();
     ViewTable modelTable = modelFactory.createView(new ViewQuery(scale));
     OutputFormFactory viewFactory = new TabularTextOutputFormFactory();
     OutputForm view = viewFactory.renderView(modelTable);
-
+    
     System.out.println(view.toString());
-
-
+    
+    
+    ChordHelper chordHelper = ChordHelper.getInstance();
+    Chord chord = chordHelper.getChord("G", "major");
     modelFactory = new GuitarViewFactory();
-    modelTable = modelFactory.createView(new ViewQuery(scale));
+    modelTable = modelFactory.createView(new ViewQuery(chord));
     viewFactory = new GridTextOutputFormFactory();
     view = viewFactory.renderView(modelTable);
-
+    
     System.out.println(view.toString());
-
   }
 
 
