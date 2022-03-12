100aa0aacf974b15a18aba5be4fb8ec94fc271d6
GK71Pro <gk71pro@gmail.com>
2022-03-12 13:37:37 -0500

    Commit Point

--- src/main/java/com/gkaraffa/guarneri/common/TestPlatform.java
+++ src/main/java/com/gkaraffa/guarneri/common/TestPlatform.java
@@ -7,7 +7,6 @@ import com.gkaraffa.cremona.helper.ScaleHelper;
 import com.gkaraffa.cremona.theoretical.Tone;
 import com.gkaraffa.cremona.theoretical.ToneGroupObject;
 import com.gkaraffa.cremona.theoretical.scale.Scale;
-import com.gkaraffa.guarneri.old.ReharmonizationOptionsAnalyticViewFactory;
 import com.gkaraffa.guarneri.outputform.CSVOutputFormFactory;
 import com.gkaraffa.guarneri.outputform.OutputForm;
 import com.gkaraffa.guarneri.outputform.OutputFormFactory;
@@ -16,10 +15,6 @@ import com.gkaraffa.guarneri.view.ViewFactory;
 import com.gkaraffa.guarneri.view.ViewQuery;
 import com.gkaraffa.guarneri.view.ViewQueryBuilder;
 import com.gkaraffa.guarneri.view.ViewTable;
-import com.gkaraffa.guarneri.view.analytic.key.ParallelModeAnalyticViewFactory;
-import com.gkaraffa.guarneri.view.analytic.scale.IntervalAnalyticViewFactory;
-import com.gkaraffa.guarneri.view.analytic.scale.RomanNumeralAnalyticViewFactory;
-import com.gkaraffa.guarneri.view.analytic.scale.StepPatternAnalyticFactory;
 import com.gkaraffa.guarneri.view.instrument.GuitarViewFactory;
 
 public class TestPlatform {
@@ -39,27 +34,25 @@ public class TestPlatform {
   }
 
   public void run() {
-    ViewQuery scaleQuery = createScaleViewQuery("C#/Db", "Aeolian");
-    ViewQuery keyQuery = createKeyViewQuery(Tone.E);
+    ToneGroupObject toneGroupObject = this.helper.getScale("C", "Ionian");
+    ViewQuery toneGroupObjectQuery = createToneGroupViewQuery(toneGroupObject);
 
-    ToneGroupObject toneGroupObject = this.helper.getScale("C#/Db", "Aeolian");
-    //ViewQuery toneGroupObjectQuery = createToneGroupViewQuery(toneGroupObject);
-    
-    List<OutputForm> forms = new ArrayList<OutputForm>();
+    List<OutputForm> forms = new ArrayList<>();
 
     //forms.add(createTabularOutputForm(new StepPatternAnalyticFactory(), scaleQuery));
     //forms.add(createTabularOutputForm(new RomanNumeralAnalyticViewFactory(), scaleQuery));
     //forms.add(createTabularOutputForm(new IntervalAnalyticViewFactory(), scaleQuery));
-    forms.add(createCSVOutputForm(new ParallelModeAnalyticViewFactory(), keyQuery));
+    //forms.add(createCSVOutputForm(new ParallelModeAnalyticViewFactory(), keyQuery));
     //forms.add(createTabularOutputForm(new ReharmonizationOptionsAnalyticViewFactory(), scaleQuery));
-    //forms.add(createCSVOutputForm(new GuitarViewFactory(), toneGroupObjectQuery));
+    forms.add(createCSVOutputForm(new GuitarViewFactory(), toneGroupObjectQuery));
 
     for(OutputForm form: forms) {
       System.out.println(form.toString());
     }
 
   }
-  
+
+  /*
   private ViewQuery createScaleViewQuery(String sKey, String sScale) {
     Scale scale = this.helper.getScale(sKey, sScale);
 
@@ -68,21 +61,23 @@ public class TestPlatform {
 
     return viewQueryBuilder.compileViewQuery();
   }
-  
+
   private ViewQuery createKeyViewQuery(Tone tKey) {
     this.viewQueryBuilder.clear();
     this.viewQueryBuilder.insertCriteria("Key", tKey);
 
     return viewQueryBuilder.compileViewQuery();
   }
-  
+  */
+
   private ViewQuery createToneGroupViewQuery(ToneGroupObject toneGroupObject) {
     this.viewQueryBuilder.clear();
     this.viewQueryBuilder.insertCriteria("ToneGroupObject", toneGroupObject);
 
     return viewQueryBuilder.compileViewQuery();
   }
-
+  
+  /*
   private OutputForm createTabularOutputForm(ViewFactory viewFactory, ViewQuery viewQuery) {
     ViewTable viewTable = viewFactory.createView(viewQuery);
     OutputFormFactory formFactory = new TabularTextOutputFormFactory();
@@ -90,7 +85,8 @@ public class TestPlatform {
     return formFactory.renderView(viewTable);
 
   }
-  
+  */
+
   private OutputForm createCSVOutputForm(ViewFactory viewFactory, ViewQuery viewQuery) {
     ViewTable viewTable = viewFactory.createView(viewQuery);
     OutputFormFactory formFactory = new CSVOutputFormFactory();
@@ -98,6 +94,5 @@ public class TestPlatform {
     return formFactory.renderView(viewTable);
 
   }
-  
-  
+
 }
